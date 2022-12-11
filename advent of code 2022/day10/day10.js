01b3ac9377abb30400f102310221eccc291e9b0d
MichaelPCgamer98 <119554773+MichaelPCgamer98@users.noreply.github.com>
2022-12-10 22:03:07 -0500

    Update day10.js

--- advent of code 2022/day10/day10.js
+++ advent of code 2022/day10/day10.js
@@ -26,8 +26,10 @@ for (let d of data) {
 
     for (let i = 1; i <= 2; i++) {
       if (i === 1) console.log("Start cycle  " + cycle + ": begin executing " + dArr);
-      if (PT2.length === x - 1 || PT2.length === x || PT2.length === x + 1) PT2 += "#";
-      else PT2 += ".";
+
+      if ((PT2.length % 40) === x - 1 || (PT2.length % 40) === x || (PT2.length % 40) === x + 1) {
+        PT2 += "#";
+      } else PT2 += ".";
       if (wantedCycles.has(cycle)) {
         // console.log("cycle " + cycle + " x " + x);
         // console.log(cycle * x);
@@ -38,10 +40,11 @@ for (let d of data) {
         console.log("Start cycle  " + cycle + ": begin executing " + dArr);
         x += parseInt(dArr[1]);
       }
+
       console.log(PT2);
       cycle++;
     }
-    console.log("fiinish execiting " + dArr + " (Register X is now " + x + ")");
+    console.log("fiinish executing " + dArr + " (Register X is now " + x + ")");
   } else if (dArr[0] === "noop") {
     console.log("Start cycle  " + cycle + ": begin executing " + dArr);
 
@@ -50,8 +53,9 @@ for (let d of data) {
       // console.log(cycle * x);
       ans += (cycle * x);
     }
-    if (PT2.length === x - 1 || PT2.length === x || PT2.length === x + 1) PT2 += "#";
-    else PT2 += ".";
+    if ((PT2.length % 40) === x - 1 || (PT2.length % 40) === x || (PT2.length % 40) === x + 1) {
+      PT2 += "#";
+    } else PT2 += ".";
     console.log(PT2);
     cycle++;
   }
@@ -64,13 +68,15 @@ console.log(ans);
 
 // 13740 is correct!
 
-// let PT2Pieces = [];
+let PT2Pieces = [];
 
-// const chunkSize = 40;
-// for (let i = 0; i < PT2.length; i += chunkSize) {
-//   const chunk = PT2.slice(i, i + chunkSize);
-//   console.log(chunk.join(""))
-//   PT2Pieces.push(chunk.join(""));
-// }
+const chunkSize = 40;
+for (let i = 0; i < PT2.length; i += chunkSize) {
+  const chunk = PT2.slice(i, i + chunkSize);
+  console.log(chunk)
+  PT2Pieces.push(chunk);
+}
 
-// console.log(PT2Pieces);
+console.log(PT2Pieces);
+
+// PT2 ZUPRFECL is correct!
